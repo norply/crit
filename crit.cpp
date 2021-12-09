f4df9cea4b561f94b1f728ea070dde97397c5f20
norply <75418405+norply@users.noreply.github.com>
2021-12-09 23:30:37 +0500

    Начало
    Добавляем библиотеки для ввода/вывода, использования русского языка, добавляем русский язык.

---
+++ crit.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	setlocale(LC_CTYPE, "rus");
+}
