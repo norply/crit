0ac9ac73daf93404a104331dd1643a8e56799d3e
norply <75418405+norply@users.noreply.github.com>
2021-12-09 23:36:37 +0500

    Ввод количества критериев
    Добавляем ввод количества критериев, проверку ввода. Создаём массив, в котором будут храниться данные попарного сравнения критериев.

--- crit.cpp
+++ crit.cpp
@@ -7,4 +7,33 @@ int main()
 	SetConsoleOutputCP(1251);
 	setlocale(LC_ALL, "Russian");
 	setlocale(LC_CTYPE, "rus");
+	float num;// количество критериев
+	for (;;)//проверка ввода количества критериев
+	{
+		cout << "Введите количество критериев:";
+		cin >> num;
+		if (!cin)
+		{
+			cout << "Введите число"<<endl;
+		}
+		else
+		{
+			if (num <= 0)
+			{
+				cout << "Введите положительное число" << endl;
+			}
+			else
+			{
+				if (int(num) != num)
+				{
+					cout << "Введите целое число" << endl;
+				}
+				else
+					break;
+			}
+		}
+		cin.clear();
+		cin.ignore(100000, '\n');
+	}
+	float* a = new float[num*num];
 }
